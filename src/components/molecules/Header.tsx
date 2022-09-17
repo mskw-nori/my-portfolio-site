5b11f513890759ad132df6bf9c7cce4ee0a5e559
mskw-nori <desudesugi@gmail.com>
2022-09-17 19:43:12 +0900

    add github link header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 
 import styles from '@/styles/modules/Header.module.scss'
 
+import { GithubIcon } from '@/components/atoms/icons/GithubIcon'
+
 import type { FC } from 'react'
 
 type Props = {
@@ -11,12 +13,17 @@ type Props = {
 export const Header: FC<Props> = ({ pageName }) => (
   <header className={styles.header}>
     <h1 className={styles.header__title}>{pageName}</h1>
-    <div>
+    <div className={styles.header__links}>
       <Link href="/">
         <a className={styles.header__link}>HOME</a>
       </Link>
       <Link href="/about">
         <a className={styles.header__link}>ABOUT</a>
+      </Link>
+      <Link href="https://github.com/mskw-nori">
+        <a className={styles.header__link} target="_blank">
+          <GithubIcon fill="#fff" width="30px" height="30px" />
+        </a>
       </Link>
     </div>
   </header>
