8f701ef4acfdd16504db06ab51b10bdfc289cc9d
mskw-nori <desudesugi@gmail.com>
2022-09-20 11:31:11 +0900

    add style  variation  to header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,4 +1,6 @@
+import clsx from 'clsx'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import styles from '@/styles/modules/Header.module.css'
 
@@ -6,28 +8,33 @@ import { GithubIcon } from '@/components/atoms/icons/GithubIcon'
 
 import type { FC } from 'react'
 
-type Props = {
-  pageName: string
-}
+const links = [
+  { href: '/', name: 'HOME' },
+  { href: '/about', name: 'ABOUT' },
+  { href: '/skills', name: 'SKILLS' }
+]
 
-export const Header: FC<Props> = ({ pageName }) => (
-  <header className={styles.header}>
-    <h1 className={styles.header__title}>{pageName}</h1>
-    <div className={styles.header__links}>
-      <Link href="/">
-        <a className={styles.header__link}>HOME</a>
-      </Link>
-      <Link href="/about">
-        <a className={styles.header__link}>ABOUT</a>
-      </Link>
-      <Link href="/skills">
-        <a className={styles.header__link}>SKILLS</a>
-      </Link>
-      <Link href="https://github.com/mskw-nori">
-        <a className={styles.header__link} target="_blank">
-          <GithubIcon fill="#fff" width="30px" height="30px" />
-        </a>
-      </Link>
-    </div>
-  </header>
-)
+export const Header: FC = () => {
+  const router = useRouter()
+  console.log(router.pathname)
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__contents}>
+        <div className={styles.header__links}>
+          {links.map(({ href, name }) => (
+            <Link href={href} key={href}>
+              <a className={clsx([styles.header__link, router.pathname === href && styles['header__link--current']])}>{name}</a>
+              {/* <a className={`${styles.header__link}${router.pathname === href ? ` ${styles['header__link--current']}` : ''}`}>{name}</a> */}
+            </Link>
+          ))}
+          <Link href="https://github.com/mskw-nori">
+            <a className={styles.header__link} target="_blank">
+              <GithubIcon width="30px" />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
