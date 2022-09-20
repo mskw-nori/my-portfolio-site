786ab98d16fff41626eefb209db86ce596294d7d
mskw-nori <desudesugi@gmail.com>
2022-09-20 11:34:36 +0900

    add svg icons content in clipboard

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -1,25 +1,96 @@
 import styles from '@/styles/modules/Skills.module.css'
 
+import { PorfileImage } from '@/components/atoms/PorfileImage'
+import { CssIcon } from '@/components/atoms/icons/CssIcon'
+import { GitIcon } from '@/components/atoms/icons/GitIcon'
+import { GithubIcon } from '@/components/atoms/icons/GithubIcon'
+import { HtmlIcon } from '@/components/atoms/icons/HtmlIcon'
+import { JavaScriptIcon } from '@/components/atoms/icons/JavaScriptIcon'
+import { ReactIcon } from '@/components/atoms/icons/ReactIcon'
+import { SassIcon } from '@/components/atoms/icons/SassIcon'
+import { TypeScriptIcon } from '@/components/atoms/icons/TypeScriptIcon'
+import { WebpackIcon } from '@/components/atoms/icons/WebpackIcon'
 import { Clipboard } from '@/components/molecules/Clipboard'
-import { ProfileImageWithName } from '@/components/molecules/ProfileImageWithName'
 import { MainWithHeaderAndFooter } from '@/components/templates/MainWithHeaderAndFooter'
 
 import type { NextPageWithLayout } from '@/types/app'
 
 const content = [
-  { title: 'hogehogehoge', description: 'foofoofoo' },
-  { title: 'hogehogehoge', description: 'foofoofoo' },
-  { title: 'hogehogehoge', description: 'foofoofoo' },
-  { title: 'hogehogehoge', description: 'foofoofooaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssssssss' },
-  { title: 'hogehogehoge', description: 'foofoofoo' }
+  {
+    title: (
+      <>
+        ・html/css <HtmlIcon height="24px" />
+        <CssIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  },
+  {
+    title: (
+      <>
+        ・Sass <SassIcon height="22px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  },
+  {
+    title: (
+      <>
+        ・JavaScript <JavaScriptIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  },
+  {
+    title: (
+      <>
+        ・TypeScript <TypeScriptIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  },
+  {
+    title: (
+      <>
+        ・Webpack <WebpackIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofooaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
+  },
+  {
+    title: (
+      <>
+        ・React <ReactIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  },
+  {
+    title: (
+      <>
+        ・Git/Github <GitIcon height="24px" />
+        <GithubIcon height="24px" />
+      </>
+    ),
+    description: 'foofoofoo'
+  }
 ]
 
 const Skills: NextPageWithLayout = () => (
   <section className={styles.skill_page}>
     <div className={styles.skill_page__profile}>
-      <ProfileImageWithName pageComment="スキルページ" />
+      <PorfileImage />
     </div>
-    <Clipboard content={content} title="Skills" />
+    <Clipboard name="Skills">
+      <ol>
+        {content.map(({ title, description }, index) => (
+          <li key={index}>
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </li>
+        ))}
+      </ol>
+    </Clipboard>
   </section>
 )
 
