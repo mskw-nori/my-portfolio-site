c43cf72fef5429c6f15b892f03f61260cafad657
mskw-nori <desudesugi@gmail.com>
2022-10-09 00:02:57 +0900

    change background-image style and background icon

--- src/components/templates/MainWithHeaderAndFooter.tsx
+++ src/components/templates/MainWithHeaderAndFooter.tsx
@@ -1,4 +1,8 @@
+import { useRouter } from 'next/router'
+
 import styles from '@/styles/modules/MainWithHeaderAndFooter.module.css'
+
+import { ClipboardIcon } from '@/components/atoms/icons/ClipboardIcon'
 
 import { Footer } from '@/components/molecules/Footer'
 import { Header } from '@/components/molecules/Header'
@@ -9,10 +13,18 @@ type Props = {
   children: ReactNode
 }
 
-export const MainWithHeaderAndFooter: FC<Props> = ({ children }) => (
-  <div className={styles.layout}>
-    <Header />
-    <main className={styles.layout__container}>{children}</main>
-    <Footer />
-  </div>
-)
+export const MainWithHeaderAndFooter: FC<Props> = ({ children }) => {
+  const router = useRouter()
+  const isHome = router.pathname === '/'
+
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.layout__container}>
+        {children}
+        {!isHome && <ClipboardIcon className={styles.layout__icon} width={150} fill="#B7CDE8" />}
+      </main>
+      <Footer />
+    </div>
+  )
+}
